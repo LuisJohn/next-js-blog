874ff30f6387fd402abf858076e6a500fcfb5533
Luis John <mnrdzjohn@gmail.com>
2024-04-15 13:58:59 +0200

    Mobile Responsive issue fixed

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
 
   console.log(data);
   return (
-    <div className="grid grid-cols-2 md:grid-cols-2  mt-5 gap-5">
+    <div className="grid grid-cols-1 md:grid-cols-2  mt-5 gap-5">
       {data.map((post, idx)=>(
         <Card key={idx}>
           <Image 
